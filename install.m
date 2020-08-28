e3b2976c78773f8346c544be253e9599ebdb861d
Dongdong Kong <kongdd@users.noreply.github.com>
2020-08-29 01:32:06 +0800

    add install.m

---
+++ install.m
@@ -0,0 +1,10 @@
+% addpath MATLAB
+% addpath MATLAB/PML
+% addpath MATLAB/pkg
+
+addpath("base")
+addpath("plot")
+addpath("plyr")
+addpath("smoother")
+
+savepath;
--- push_gitlab.bat
+++
@@ -1,4 +0,0 @@
-git remote set-url origin https://gitlab.com/hydro2/MATLAB_pkgs
-git push -f
-
-git remote set-url origin https://github.com/kongdd/MATLAB_pkgs
